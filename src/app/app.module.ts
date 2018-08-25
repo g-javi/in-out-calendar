f18b2fc58ede8f99fd1b7eeb247842dba0c2edc6
Glen Javier <glen02javier@gmail.com>
2018-08-25 11:36:10 +1000

    feature: updated @angular/material to latest version, added navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, MatTableModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppNavComponent } from './components/app-nav/app-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
-import { BodyComponent } from './componets/body/body.component';
+import { BodyComponent } from './components/body/body.component';
+import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
   declarations: [
@@ -16,6 +16,8 @@ import { BodyComponent } from './componets/body/body.component';
     BodyComponent
   ],
   imports: [
+    MatTableModule,
+    CdkTableModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
--- src/app/components/app-nav/app-nav.component.ts
+++ src/app/components/app-nav/app-nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-nav',
@@ -10,11 +11,81 @@ import { map } from 'rxjs/operators';
 })
 export class AppNavComponent {
 
+  dates$: any[];
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  displayedColumns;
+  dataSource = [
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' },
+    { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }, { name: '', star: '' }
+  ];
+  constructor(private breakpointObserver: BreakpointObserver) {
+    // const fromDate = moment();
+    // const toDate = moment().add(7, 'days');
+    const fromDate = moment().startOf('month').week();
+    console.log(fromDate);
+    const toDate = moment().endOf('month').week();
+    console.log(toDate);
 
+    const calendar = [];
+    for (let week = fromDate; week < toDate; week++) {
+      calendar.push({
+        week: week,
+        days: Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day'))
+      });
+    }
+
+    console.log(calendar);
+    // window['moment'] = moment;
+    this.dates$ = this.getMonthDays(moment().format('MMM'), 2018); // this.enumerateDaysBetweenDates(fromDate, toDate);
+    console.log(this.dates$);
+    this.displayedColumns = ['name'].concat(this.dates$);
+
+    console.log(this.displayedColumns);
+  }
+
+  getMonthDays(month, year) {
+    const ar = [];
+    // tslint:disable-next-line:quotemark
+    const start = moment(year + '-' + month, "YYYY-MMM");
+    for (const end = moment(start).add(1, 'month'); start.isBefore(end); start.add(1, 'day')) {
+      ar.push(start.format('DD/MM/YYYY'));
+    }
+    return ar;
+  }
+
+  enumerateDaysBetweenDates(startDate, endDate) {
+    const now = startDate, dates = [];
+
+    while (now.isSameOrBefore(endDate)) {
+      dates.push(now.format('M/D/YYYY'));
+      now.add(1, 'days');
+    }
+    return dates;
+  }
 }
